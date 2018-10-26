31a0a20f7930bfcfb8e50b29ba5e64d76203f88f
Alin Popa <alin.popa@gmail.com>
2018-10-26 09:36:07 +0100

    Prepare for first release.

--- mix.exs
+++ mix.exs
@@ -6,10 +6,31 @@ defmodule Exvalid.MixProject do
       app: :exvalid,
       version: "0.1.0",
       elixir: "~> 1.7",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: [
         {:witchcraft, "~> 1.0"}
-      ]
+      ],
+      name: "ExValid",
+      source_url: "https://github.com/alinpopa/exvalid"
+    ]
+  end
+
+  def application() do
+    []
+  end
+
+  defp description() do
+    "Collecting errors validation ADT using Witchcraft."
+  end
+
+  defp package() do
+    [
+      maintainers: ["Alin Popa"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/alinpopa/exvalid"}
     ]
   end
 end
